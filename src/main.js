d4bd49a97a1fbd0256e07a338681b7c1887887e0
Shuopeng Zhou <zsp322@gmail.com>
2018-02-09 00:15:15 -0500

    1.Main Page added 2.Router Fixed

--- src/main.js
+++ src/main.js
@@ -3,14 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+Vue.use(MintUI);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   components: { App },
   template: '<App/>'
 });
