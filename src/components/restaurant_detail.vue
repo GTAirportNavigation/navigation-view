1b91366d46aa29911c845f7ec9f4e57f8fe2af6a
Jiashen Cao <caojiashen24@gmail.com>
2018-03-01 22:47:26 -0500

    restaurants detail view

---
+++ src/components/restaurant_detail.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-container>
+    <el-main style="padding: 0; text-align: left;">
+      <el-row style="margin-top: 2em; text-align: center">
+        <img :src="require('./../assets/fiveguys.jpg')" width="80%"/>
+      </el-row>
+      <el-row>
+        Yelp Rating: {{score}} / 5
+      </el-row>
+      <el-row>
+        Located: {{location}}
+      </el-row>
+      <el-row style="text-align: center">
+        <el-col :span="11"><el-button class="cancel-button">Cancel</el-button></el-col>
+        <el-col :span="13"><el-button class="start-button">Start</el-button></el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+    var data = {
+      score: 3.5,
+      location: 'Located near Gate 45'
+    };
+
+    export default {
+      name: "restaurant_detail",
+      data: function() {
+        return data;
+      }
+    }
+</script>
+
+<style scoped>
+  .el-row {
+    margin-top: 1em;
+    padding: 1em;
+  }
+
+  .cancel-button {
+    color: white;
+    background-color: orangered;
+    border: orangered;
+  }
+
+  .start-button {
+    color: white;
+    background-color: dodgerblue;
+    border: dodgerblue;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import home from '@/components/home'
 import select_page from '@/components/select'
 import MainPage from '@/components/MainPage'
 import restaurants from '@/components/restaurants'
+import restaurant_detail from '@/components/restaurant_detail'
 
 Vue.use(Router);
 
@@ -27,8 +28,13 @@ export default new Router({
     },
     {
       path: '/restaurants',
-      name: 'restaurants_overview',
+      name: 'restaurants-overview',
       component: restaurants
+    },
+    {
+      path: '/restaurant-detail',
+      name: 'restaurants-detail',
+      component: restaurant_detail
     }
   ]
 })
